393ab71798d6b83cf649b1edf38209dec9676112
Natanael Nunes <natanaelfpnunes@gmail.com>
2021-09-19 15:46:01 -0300

    edit: renomeando o projeto de desktop

--- Desktop/BlackRiver.Desktop/App.xaml.cs
+++ Desktop/BlackRiver.Desktop/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace BlackRiverDesktop
+namespace BlackRiver.Desktop
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- Desktop/BlackRiver.Desktop/MainWindow.xaml.cs
+++ Desktop/BlackRiver.Desktop/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace BlackRiverDesktop
+namespace BlackRiver.Desktop
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
